788c33165744cfb6d868dbd964fd7f28d85cabc6
Aaron Chafetz <achafetz@usaid.gov>
2020-03-12 15:30:17 -0400

    fix date

--- Scripts/02_datim_flags.R
+++ Scripts/02_datim_flags.R
@@ -119,7 +119,7 @@
   
   
   #store file name
-  filename <- paste0("DATIM_FLAGS_20200312", format(Sys.Date(), "%Y%m%d"), ".csv")
+  filename <- paste0("DATIM_FLAGS", format(Sys.Date(), "%Y%m%d"), ".csv")
   
   #save
   write_csv(df_datim_wgts, file.path(out_folder, filename), na = "")
