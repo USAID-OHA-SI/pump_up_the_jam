7f3d0ab4dcaf9b9447e095562386a6579fe0a88d
Aaron Chafetz <achafetz@usaid.gov>
2020-03-13 12:13:11 -0400

    plot completeness by period

--- Scripts/93_viz_completeness.R
+++ Scripts/93_viz_completeness.R
@@ -26,11 +26,19 @@ ind_sel  <- c("HTS_TST", "TX_NEW", "TX_CURR")
   df_completeness_viz <- list.files(out_folder, "HFR_Completeness_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
     vroom()
 
+  df_completeness_pds_viz <- list.files(out_folder, "HFR_Completeness_Pds_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
+    vroom()
 
 # MUNGE -------------------------------------------------------------------
 
 #arrange
   df_completeness_viz <- df_completeness_viz %>% 
+    mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
+                                     operatingunit == "Dominican Republic" ~ "DR",
+                                     operatingunit == "Western Hemisphere Region" ~ "WH Region",
+                                     TRUE ~ operatingunit))
+  
+  df_completeness_pds_viz <- df_completeness_pds_viz %>% 
     mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
                                      operatingunit == "Dominican Republic" ~ "DR",
                                      operatingunit == "Western Hemisphere Region" ~ "WH Region",
@@ -61,4 +69,29 @@ ind_sel  <- c("HTS_TST", "TX_NEW", "TX_CURR")
           strip.text = element_text(family = "Calibri", face = "bold"),
           #panel.grid.major.y = element_line(color = "gray50"),
           legend.position = "bottom",
+          plot.caption = element_text(color = "gray30"))
+  
+  
+  
+  df_completeness_pds_viz %>% 
+    filter(indicator %in% ind_sel) %>% 
+    mutate(indicator = factor(indicator, ind_sel)) %>% 
+    ggplot(aes(hfr_pd, fct_reorder(operatingunit, is_datim_site, .fun = sum), fill = completeness)) +
+    geom_tile(color = "white") +
+    geom_text(aes(label = percent(completeness)), size = 3, 
+              color = "#303030", family = "Calibri Light") +
+    scale_fill_viridis_c(option = "A", begin = 0.5, na.value = "#f1f1f1", direction = -1) +
+    facet_grid(site_type ~ indicator, switch = "y") +
+    labs(title = "FOCUSING ON IMPORTANT SITES PROVIDES BETTER REPORTING COMPLETESS",
+         subtitle = "FY20Q1 Site x Mechanism HFR Reporting Completeness", 
+         y = NULL, x = NULL, color = "Site Type",
+         caption = "Note: Completeness derived by comparing HFR reporting against sites with DATIM results/targets
+         Source: FY20Q1 MER + HFR") +
+    theme_minimal() +
+    theme(text = element_text(family = "Calibri Light"),
+          plot.title = element_text(family = "Calibri", face = "bold"),
+          strip.text = element_text(family = "Calibri", face = "bold"),
+          strip.placement = "outside",
+          panel.grid = element_blank(),
+          legend.position = "none",
           plot.caption = element_text(color = "gray30"))
