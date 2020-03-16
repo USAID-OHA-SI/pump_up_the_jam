ef4c57315bbe3d2bdc6c5979d579b43948bb15b9
Aaron Chafetz <achafetz@usaid.gov>
2020-03-16 11:02:37 -0400

    incorporate TE adj to 1st graphic

--- Scripts/93_viz_completeness.R
+++ Scripts/93_viz_completeness.R
@@ -3,7 +3,7 @@
 ## LICENSE:  MIT
 ## PURPOSE:  visualizing completeness of reporting
 ## DATE:     2020-03-13
-## UPDATED:  
+## UPDATED:  2020-03-16
 
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -13,6 +13,7 @@ library(lubridate)
 library(vroom)
 library(scales)
 library(extrafont)
+library(ggtext)
 
 # GLOBAL VARIABLES --------------------------------------------------------
 
@@ -21,68 +22,103 @@ viz_folder <- "Images"
 
 ind_sel  <- c("HTS_TST", "TX_NEW", "TX_CURR")
 
-color_hv_sites <- viridis_pal()(1)
+pal <- viridis_pal()(6) #%>% show_col()
+color_hv_sites <- pal[1]
+color_ref <- pal[4]
 color_all_sites <- "#D3D3D3"
 
 # IMPORT ------------------------------------------------------------------
 
-
-  df_completeness_viz <- list.files(out_folder, "HFR_Completeness_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
-    vroom()
-
-  df_completeness_pds_viz <- list.files(out_folder, "HFR_Completeness_Pds_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
-    vroom()
+  #import
+    df_completeness_viz <- list.files(out_folder, "HFR_Completeness_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
+      vroom()
+  
+    df_completeness_pds_viz <- list.files(out_folder, "HFR_Completeness_Pds_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
+      vroom()
 
 # MUNGE -------------------------------------------------------------------
 
-#arrange
-  df_completeness_viz <- df_completeness_viz %>% 
-    mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
-                                     operatingunit == "Dominican Republic" ~ "DR",
-                                     operatingunit == "Western Hemisphere Region" ~ "WH Region",
-                                     TRUE ~ operatingunit))
+  #clean up OU names
+    df_completeness_viz <- df_completeness_viz %>% 
+      mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
+                                       operatingunit == "Dominican Republic" ~ "DR",
+                                       operatingunit == "Western Hemisphere Region" ~ "WH Region",
+                                       TRUE ~ operatingunit))
   
-  df_completeness_pds_viz <- df_completeness_pds_viz %>% 
-    mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
-                                     operatingunit == "Dominican Republic" ~ "DR",
-                                     operatingunit == "Western Hemisphere Region" ~ "WH Region",
-                                     TRUE ~ operatingunit))
-  
-  
-  df_completeness_pds_viz <- df_completeness_pds_viz %>% 
-    mutate(class = case_when(completeness < .6 ~ "low",
-                             completeness < .9 ~ "med",
-                             TRUE              ~ "okay"))
+  #create a global reference line (OU averages - All sites)
+    df_ref_completeness_avg <- df_completeness_viz %>%
+        filter(site_type == "All") %>% 
+        group_by(indicator) %>%
+        summarise(completeness_global_ou_avg = mean(completeness, na.rm = TRUE)) %>% 
+        ungroup()
+    
+  #create a global reference line (global totals - All sites)
+    df_ref_completeness <- df_completeness_viz %>%
+      filter(site_type == "All") %>% 
+      group_by(indicator) %>%
+      summarise_at(vars(has_hfr_reporting, is_datim_site), sum, na.rm = TRUE) %>% 
+      ungroup() %>% 
+      mutate(completeness_global = has_hfr_reporting / is_datim_site) %>% 
+      select(indicator, completeness_global)
+    
+  #merge
+    df_completeness_viz <- df_completeness_viz %>% 
+      left_join(df_ref_completeness_avg) %>% 
+      left_join(df_ref_completeness)
+    
+    
+  #clean up OU names
+    df_completeness_pds_viz <- df_completeness_pds_viz %>% 
+      mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
+                                       operatingunit == "Dominican Republic" ~ "DR",
+                                       operatingunit == "Western Hemisphere Region" ~ "WH Region",
+                                       TRUE ~ operatingunit))
+    
+  #classes for completeness
+    df_completeness_pds_viz <- df_completeness_pds_viz %>% 
+      mutate(class = case_when(completeness < .6 ~ "low",
+                               completeness < .9 ~ "med",
+                               TRUE              ~ "okay"))
 
 # PLOT --------------------------------------------------------------------
   
 
-  df_completeness_viz %>% 
+  theme_set(theme_minimal(base_family = "Source Sans Pro"))
+  
+  my_title <- "<b><span style = 'color:#440154;'>HIGH VOLUME SITES</span></b> ARE REPORTING AT HIGHER LEVELS THAN OTHER SITES</span></b>"
+  
+  df_completeness_viz %>%
     filter(indicator %in% ind_sel,
-           site_type != "Low Volume (Target)") %>% 
-    mutate(indicator = factor(indicator, ind_sel)) %>% 
-    ggplot(aes(fct_reorder(operatingunit, is_datim_site, .fun = sum), completeness, group = operatingunit, color = site_type)) +
+           site_type != "Low Volume (Target)") %>%
+    group_by(operatingunit) %>%
+    mutate(sort_max = if_else(site_type =="High Volume (Target)" & indicator == "HTS_TST", completeness, NA_real_)) %>%
+    fill(sort_max, .direction = "updown") %>%
+    ungroup() %>%
+    mutate(indicator = factor(indicator, ind_sel),
+           ou_sort = fct_reorder(operatingunit, sort_max)) %>%
+    ggplot(aes(ou_sort, completeness, group = operatingunit, color = site_type)) +
     geom_hline(aes(yintercept = 0), color = "gray40") +
-    geom_hline(aes(yintercept = 1), color = "gray40", linetype = "dashed") +
+    geom_hline(aes(yintercept = 1), color = "gray40") + #, linetype = "dashed"
+    geom_hline(aes(yintercept = completeness_global), color = color_ref, linetype = "dashed", size = .9) +
     geom_path(size = .6, color = 'gray70') +
     geom_point(size = 4) +
     scale_y_continuous(labels = percent) +
     scale_color_manual(values = c(color_all_sites, color_hv_sites)) +
     coord_flip() +
     facet_grid(~ indicator) +
-    labs(title = "FOCUSING ON IMPORTANT SITES PROVIDES BETTER REPORTING COMPLETESS",
-         subtitle = "FY20Q1 Site x Mechanism HFR Reporting Completeness", 
+    labs(title = my_title,
+         subtitle = "FY20Q1 Site x Mechanism HFR Reporting Completeness",
          y = NULL, x = NULL, color = "Site Type",
-         caption = "Notes: 
+         caption = "Notes:
          (a) Completeness derived by comparing HFR reporting against sites with DATIM results/targets
-         (b) OUs sorted by the number of sites x mechanism pairs 
+         (b) OUs sorted by HTS_TST completeness
+         (c) The green dashed line denoted global completeness of reporting
          Source: FY20Q1 MER + HFR") +
     theme_minimal() +
-    theme(text = element_text(family = "Calibri Light"),
-          plot.title = element_text(family = "Calibri", face = "bold"),
-          strip.text = element_text(family = "Calibri", face = "bold"),
-          #panel.grid.major.y = element_line(color = "gray50"),
-          legend.position = "bottom",
+    theme(strip.text = element_text(hjust = 0),
+          legend.position = "top",
+          legend.justification = c(0, 0),
+          plot.title = element_markdown(hjust = 0, size = 14, face = "bold", color = "gray30"),
           plot.caption = element_text(color = "gray30"))
   
   ggsave(file.path(viz_folder,"HFR_Completeness.png"), dpi = 300, 
