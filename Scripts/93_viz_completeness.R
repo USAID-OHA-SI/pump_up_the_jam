7d764fadad6c80790fc5a438ac7bccbf23de1a50
tim <tim.essam@gmail.com>
2020-03-17 11:35:30 -0400

    functionalize sparkline munges and function

--- Scripts/93_viz_completeness.R
+++ Scripts/93_viz_completeness.R
@@ -14,8 +14,7 @@ library(vroom)
 library(scales)
 library(extrafont)
 library(ggtext)
-library(patchwork) # combining plots
-library(ggpubr) # for ggarrange
+
 
 # GLOBAL VARIABLES --------------------------------------------------------
 
@@ -38,8 +37,6 @@ color_all_sites <- "#D3D3D3"
     df_completeness_pds_viz <- list.files(out_folder, "HFR_Completeness_Pds_[[:digit:]]+\\.csv", full.names = TRUE) %>% 
       vroom()
     
-    df_completeness_pds_viz <- list.files(out_folder, "HFR_Completeness_Pds_20200316.csv", full.names = TRUE) %>% 
-      vroom()
 
 # MUNGE -------------------------------------------------------------------
 
@@ -89,33 +86,72 @@ color_all_sites <- "#D3D3D3"
     # Run lowess regression on each indicator + site type with time as the xvar.
     # Saving results so we can make an arrow at the end of the fitted line with geom_segement
     # NOTE: Didn't work out as expect, simply adding a circle at the end of fitted line
-    model_data <- df_completeness_pds_viz %>% 
-      mutate(timevar = as.numeric(hfr_pd))
+    # Functions to allow for filtering on the data frames going into the flow process
     
-    models <- model_data  %>%
-      filter(completeness > 0) %>% 
-      tidyr::nest(-site_type, - indicator) %>%
-      mutate(
-        fit = purrr:: map(data, ~ loess(completeness ~ timevar, weights = mer_targets, span = 0.75, data = .)),
-        # Extract out fitted models
-        fitted = purrr::map(fit, `[[`, "fitted")
-      )
+    prep_fitline <- function(df) {
+      
+      # Convert to numeric so it can be used in the local regression as a covariate
+      model_data <- df %>% mutate(timevar = as.numeric(hfr_pd))
+      
+      models <- model_data  %>%
+        tidyr::nest(-site_type, - indicator) %>%
+        mutate(
+          fit = purrr:: map(data, ~ loess(completeness ~ timevar, weights = mer_targets, span = 0.75, data = .)),
+          # Extract out fitted models
+          fitted = purrr::map(fit, `[[`, "fitted")
+        )
+      
+      df_results <- models %>%
+        dplyr::select(-fit) %>%
+        tidyr::unnest(cols = c(data, fitted))
+      
+      return(df_results)
+    }
+      
+    collapse_results <- function(df) {
     
-    df_results <- models %>%
-      dplyr::select(-fit) %>%
-      tidyr::unnest(cols = c(data, fitted))
+      # Collapsing down results to get the placement of the final point for the dot at end of line
+      # Called in the ggplot function
+      df_results_collapse <- df %>% 
+        filter(indicator %in% ind_sel,
+          site_type != "Low Volume (Target)") %>% 
+        group_by(indicator, site_type, hfr_pd) %>% 
+        summarise(arrows = mean(fitted)) %>% 
+        filter(hfr_pd == max(hfr_pd)) %>% 
+        mutate(grp = paste(site_type, indicator),
+          hfr_pd = as.character(hfr_pd)) %>% 
+        ungroup()
+      
+      return(df_results_collapse)
+    }
     
-    df_results_collapse <- 
-      df_results %>% 
+    sparkline_prep <- function(df) {
+    
+    df_hmap_bottom <- df %>%
       filter(indicator %in% ind_sel,
         site_type != "Low Volume (Target)") %>% 
-      group_by(indicator, site_type, hfr_pd) %>% 
-      summarise(arrows = mean(fitted)) %>% 
-      filter(hfr_pd == max(hfr_pd)) %>% 
-      mutate(grp = paste(site_type, indicator),
-        hfr_pd = as.character(hfr_pd)) %>% 
-      ungroup()
+      group_by(operatingunit) %>%
+      mutate(sort_max = ifelse(site_type =="High Volume (Target)" & indicator == "HTS_TST", completeness, NA_real_)) %>%
+      fill(sort_max, .direction = "updown") %>%
+      ungroup() %>%
+      mutate(indicator = factor(indicator, ind_sel),
+        ou_sort = fct_reorder(operatingunit, sort_max),
+        rank = percent_rank(completeness),
+        hfr_pd = as.character(hfr_pd),
+        grp = paste(site_type, indicator)
+      ) 
+  }
+   
+    # Prep data frames needed for sparklines; Collapse function called within ggplot function 
+    df_results_nonzero_sparkline <- prep_fitline(df_completeness_pds_viz %>% filter(completeness > 0)) %>%
+      sparkline_prep()
 
+   df_results_sparkline <- prep_fitline(df_completeness_pds_viz) %>% 
+     sparkline_prep()
+
+     
+  
+  
 # PLOT --------------------------------------------------------------------
   
 
@@ -174,7 +210,7 @@ color_all_sites <- "#D3D3D3"
     )
   
   
-  (df_hmap_top %>% 
+  df_hmap_top %>% 
     ggplot(aes(hfr_pd, ou_sort, fill = completeness)) +
     geom_tile(color = "white", size = 0.25) +
     geom_text(aes(label = ifelse(rank < 0.50, percent(completeness), NA_real_)),
@@ -187,13 +223,13 @@ color_all_sites <- "#D3D3D3"
          #caption = "Note: Completeness derived by comparing HFR reporting against sites with DATIM results/targets
          #Source: FY20Q1 MER + HFR",
          fill = "Reporting completeness (100% = all sites reporting) ") +
-    theme_minimal() + coord_fixed(ratio = .007) +
+    theme_minimal() + 
+      coord_fixed(ratio = .007) +
     theme(legend.position = "top",
           legend.justification = c(0, 0),
           panel.grid = element_blank(),
           axis.text.x = element_text(size = 7),
           strip.text = element_text(hjust = 0))
-  )
 
   ggsave(file.path(viz_folder,"HFR_Completeness_Pd.png"), dpi = 300, 
          width = 18, height = 10)
@@ -201,48 +237,45 @@ color_all_sites <- "#D3D3D3"
 
   
   
-  df_hmap_bottom <- df_results %>%
-    filter(indicator %in% ind_sel,
-           site_type != "Low Volume (Target)") %>% 
-    group_by(operatingunit) %>%
-    mutate(sort_max = ifelse(site_type =="High Volume (Target)" & indicator == "HTS_TST", completeness, NA_real_)) %>%
-    fill(sort_max, .direction = "updown") %>%
-    ungroup() %>%
-    mutate(indicator = factor(indicator, ind_sel),
-           ou_sort = fct_reorder(operatingunit, sort_max),
-           rank = percent_rank(completeness),
-           hfr_pd = as.character(hfr_pd),
-           grp = paste(site_type, indicator)
-    ) 
+# Need two plots here, one w/ filtering out 0s one without
+  # Functionalize plot
   
- (hmap_bottom <- 
-   df_hmap_bottom %>% 
-    ggplot(aes(hfr_pd, completeness, group = grp)) +
-    geom_hline(aes(yintercept = 0), color = "gray30") +
-    geom_jitter(size = 2, width = .2, alpha = 0.60, colour = "#D0D0D0") +
-    geom_smooth(aes(weight = mer_targets), se = FALSE, color = "#303030") +
-    geom_point(data = df_results_collapse,
-     aes(x = hfr_pd, y = arrows, fill = arrows), shape = 21, size = 2.5) +
-    scale_y_continuous(labels = percent) +
-    scale_fill_viridis_c(option = "A", direction = -1, labels = percent, end = 0.9) +
-    facet_wrap(site_type ~ indicator, nrow = 1) +
-    #facet_grid(rev(site_type) ~ indicator, switch = "y") +
-     labs(y = NULL, x = NULL,
-         caption = "Notes:
+  sparkline_plot <- function(df) {
+    # df = data frame to plot the sparkline
+    # df_dot - data frame used to create the dot at end
+    
+    df %>% 
+      ggplot(aes(hfr_pd, completeness, group = grp)) +
+      geom_hline(aes(yintercept = 0), color = "gray30") +
+      geom_jitter(size = 2, width = .2, alpha = 0.60, colour = "#D0D0D0") +
+      geom_smooth(aes(weight = mer_targets), se = FALSE, color = "#303030") +
+      geom_point(data = collapse_results(df) ,
+        aes(x = hfr_pd, y = arrows, fill = arrows), shape = 21, size = 2.5) +
+      scale_y_continuous(labels = percent) +
+      scale_fill_viridis_c(option = "A", direction = -1, labels = percent, end = 0.9) +
+      facet_wrap(site_type ~ indicator, nrow = 1) +
+      #facet_grid(rev(site_type) ~ indicator, switch = "y") +
+      labs(y = NULL, x = NULL,
+        caption = "Notes:
          (a) Completeness derived by comparing HFR reporting against sites with DATIM results/targets
          (b) Each dot represents an OU's Site x Mechanism completeness for each period
          Source: FY20Q1 MER + HFR
          (c) MER targets included as weighting in polynomial fit line",
-         color = "Reporting completeness (100% = all sites reporting) ") +
-    theme_minimal() + 
-    theme(legend.position = "none",
-          legend.justification = c(0, 0),
-          axis.text.x = element_text(size = 7),
-          strip.text = element_text(hjust = 0),
-          strip.placement = "outside")
-   )
+        color = "Reporting completeness (100% = all sites reporting) ") +
+      theme_minimal() + 
+      theme(legend.position = "none",
+        legend.justification = c(0, 0),
+        axis.text.x = element_text(size = 7),
+        strip.text = element_text(hjust = 0),
+        strip.placement = "outside")
+  }
+  
+  # Return the to different plots
+  sparkline_plot(df_results_nonzero_sparkline)
+  sparkline_plot(df_results_sparkline)
+   
   
   ggsave(file.path(viz_folder,"HFR_Completeness_Pd_Trends.png"), dpi = 300, 
-         width = 18, height = 2, scale = 1.25)
+         width = 18, height = 5, scale = 1.25)
 
   
