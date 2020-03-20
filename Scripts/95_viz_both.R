2d3b2d131badb989d016708ffff29c6d290b3b3c
tim <tim.essam@gmail.com>
2020-03-20 14:13:15 -0400

    propose j2sr style plot changes

--- Scripts/95_viz_both.R
+++ Scripts/95_viz_both.R
@@ -72,8 +72,15 @@
       fill(sort_max, .direction = "updown") %>%
       ungroup() %>%
       mutate(indicator = factor(indicator, ind_sel),
-             operatingunit = fct_reorder(operatingunit, sort_max))
+             operatingunit = fct_reorder(operatingunit, sort_max)) %>% 
+    group_by(indicator, measure) %>% 
+      mutate(indicator_ave = mean(value, na.rm = TRUE)) %>% 
+      ungroup() %>% 
+      arrange(operatingunit, indicator) %>% 
+      mutate(ou_id = group_indices(., operatingunit))
 
+    #TODO: Global data frame w/ correct aggregates  
+    
 
 # PLOT --------------------------------------------------------------------
 
@@ -138,4 +145,125 @@
     
     ggsave(file.path(viz_folder,"HFR_CC_Scatter.png"), dpi = 300, 
            width = 5.625, height = 5.625)  
+    
+
+# TIM'S SUGGESTIONS - NOT SURE WHERE THEY GO ------------------------------
+   
+     # What if we mimic the J2SR plots? 
+    # Steps, create a separate data frame for the country of focus & indicator averages
+    # Flag observations above 100%, these should get colored differently
+    # Create custom color ramp that maxs at 100, then reflects previous side
+    
+    purples <- c("#edf8fb","#bfd3e6","#9ebcda","#8c96c6","#8c6bb1","#88419d","#6e016b", 
+      "#88419d", "#8c6bb1", "#8c96c6", "#9ebcda", "#bfd3e6", "#edf8fb")
+    colorRampPalette(purples)(30) %>% scales::show_col(labels = FALSE)
+    
+    
+    #TODO: FIX FACET LABELS OR RE WORK AS FACET_GRID?
+    
+    hfr_cc_scplot <- function(df, ou_code = 15) {
+      
+      df_ou <- df %>% filter(ou_id == ou_code)
+      ou_name <- df_ou %>% distinct(as.character(operatingunit)) %>% pull()
+      
+      # define max value of colors
+      ou_max <- df %>% summarise(max = max(abs(value), na.rm = TRUE)) %>% pull()
+      
+      df %>%
+        filter(ou_id != ou_code) %>%
+        ggplot(aes(x = value, y = measure)) +
+        geom_vline(data = df_ou, aes(xintercept = indicator_ave), colour = "#909090", linetype = "dotted") +
+        geom_point(size = 4, alpha = 0.70, shape = 21, colour = color_all_sites, stroke = 1, fill = color_all_sites) +
+        # geom_point(data = df_ou, aes(fill = (value <= 1)), size = 6, shape = 21, stroke = 0.5, colour = "#303030") +
+        geom_point(data = df_ou, aes(fill = value), size = 6, shape = 21, stroke = 0.5, colour = "#303030") +
+        facet_wrap(indicator ~ measure, nrow = 3, scales = "free_y") +
+        geom_text(
+          data = df_ou, aes(label = percent(value, accuracy = 1)),
+          vjust = -1.25, hjust = 0.4
+        ) +
+        scale_fill_gradientn(
+          colours = colorRampPalette(purples)(30),
+          limits = c(0, 2)
+        ) +
+        # scale_fill_manual(values = c("FALSE" = "#a180a9", "TRUE" = color_hv_sites)) +
+        scale_x_continuous(labels = percent_format(accuracy = 1), breaks = c(seq(0, 1, 0.2))) +
+        theme_minimal() +
+        theme(
+          legend.position = "none",
+          text = element_text(family = "Source Sans Pro"),
+          panel.grid = element_blank(),
+          strip.text = element_text(hjust = 0),
+          panel.spacing = unit(3, "lines")
+        ) +
+        labs(
+          x = NULL, y = NULL,
+          title = str_c(stringr::str_to_upper(ou_name))
+        )
+    }
+    
+    # List to loop over of operating unit ids
+    ou_list <- df_cc %>% distinct(ou_id) %>% pull()
+    
+    plots <- map(.x = ou_list, .f = ~ hfr_cc_scplot(df_cc, ou_code = .x))
+    plots[19][[1]]
+    
+    #TODO: CREATE GLOBAL GRAPHIC WITH RESULTS AGGREGATED TO THE APPROPRIATE LEVEL
+    
+    # To functionalize spread (inspired by happy butt day)
+    # TODO: MOVE TO GLOBALS?
+    
+    spread_scat <- function(df) {
+      df_spread <- df %>%
+        select(-indicator_ave, -ou_id) %>%
+        gather(type, value, dist, value) %>%
+        spread(measure, value) %>%
+        filter(type == "value")
+      return(df_spread)
+    }
+    
+    # Function to create country specific dot plots
+    hfr_cc_scatter <- function(df, ou_code = 15) {
+      df_scatter <- df %>%
+        filter(ou_id != ou_code) %>%
+        spread_scat()
+      
+      df_ou <- df %>%
+        filter(ou_id == ou_code) %>%
+        spread_scat()
+      
+      ou_name <- df_ou %>%
+        distinct(as.character(operatingunit)) %>%
+        pull()
+      
+      df_scatter %>%
+        ggplot(aes(correctness, completeness, size = n_sites)) +
+        geom_point(fill = color_all_sites, na.rm = TRUE, alpha = 0.75, 
+          shape = 21, colour = "white") +
+        geom_point(
+          data = df_ou, aes(size = n_sites),
+          fill = color_hv_sites, shape = 21, colour = "303030", stroke = 0.25
+        ) +
+        geom_abline(intercept = 0, slope = 1, colour = "#D3D3D3") +
+        facet_wrap(~indicator) +
+        labs(
+          title = str_c(stringr::str_to_upper(ou_name)),
+          subtitle = "FY20Q1 Site x Mechanism HFR Reporting Correctness/Completeness",
+          caption = "Source: FY20Q1 MER + HFR"
+        ) +
+        scale_y_continuous(label = percent, limits = c(0, 1.5)) +
+        scale_x_continuous(label = percent, limits =c(0, 1.5)) +
+        coord_fixed() +
+        scale_size(guide = "none") +
+        theme_minimal() +
+        theme(strip.text = element_text(hjust = 0))
+    }
+    
+    hfr_cc_scatter(df_cc, ou_code = 10)
+    hfr_scatter_plots <- map(.x = ou_list, .f = ~ hfr_cc_scatter(df_cc, ou_code = .x))
+    hfr_scatter_plots[25][[1]]
+    
+    
+    #TODO: DETERMINE BEST LAYOUT FOR THESE. MIGRATE DATA FRAMES OVER TO TABLEAU FOR TEMPLATING.
+    
+  
     
